a7d6797b97b5008469a8e3050b4ec830a833a623
Anush Kadoyan <kadoyan@usc.edu>
2015-11-21 15:41:28 -0800

    Adding functions

--- CAH/src/GUI/GamePanel.java
+++ CAH/src/GUI/GamePanel.java
@@ -1,106 +1,79 @@
 package GUI;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Insets;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
 
-import javax.swing.BorderFactory;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.*;
 
 import customUI.ImageLibrary;
 import customUI.PaintedButton;
 import customUI.PaintedPanel;
+import utilities.Card;
 
 public class GamePanel extends PaintedPanel{
 	private Image toDraw;
-
+	private static final long serialVersionUID = 1L;
+	private JPanel top, bottom, bottom1, bottom2, left, right;
+	private Image white = ImageLibrary.getImage("images/whitecard.png"); 
+	private Image black = ImageLibrary.getImage("images/blackcard.png"); 
+	private Image whiteback = ImageLibrary.getImage("images/whitecard.png"); 
+	private Image blackback = ImageLibrary.getImage("images/blackback1.png"); 
+	private Image casino = ImageLibrary.getImage("images/casino.jpg"); 
+	private Icon cigar = new ImageIcon("images/cigar.gif");
+	private JLabel cigarL = new JLabel(cigar);
+	private Icon whiskey = new ImageIcon("images/whiskey.png");
+	private JLabel whiskeyL = new JLabel(whiskey);
+	//Constructor
 	public GamePanel(Image i) {
 		super(i);
 		setLayout(new BorderLayout());
 //		PaintedPanel back = new PaintedPanel(image);
 //		add(back);
 		this.setOpaque(false);
-		Image white = ImageLibrary.getImage("images/whitecard.png"); 
-		Image black = ImageLibrary.getImage("images/blackcard.png"); 
-		Image whiteback = ImageLibrary.getImage("images/whitecard.png"); 
-		Image blackback = ImageLibrary.getImage("images/blackback1.png"); 
-
-		Image casino = ImageLibrary.getImage("images/casino.jpg"); 
+		
+		
+		//Images and icons
+		
+		
+		
 		toDraw = white;
 		PaintedPanel wCard = new PaintedPanel(white);
 		wCard.setOpaque(false);
-		JPanel top = new JPanel();
+		top = new JPanel();
 		top.setOpaque(false);
 		
-		JPanel bottom = new JPanel();
-		JPanel bottom1 = new JPanel();
-		JPanel bottom2= new JPanel();
+		bottom = new JPanel();
+		bottom1 = new JPanel();
+		bottom2= new JPanel();
 
 		bottom.setLayout(new BorderLayout());
 		bottom.add(bottom1,BorderLayout.NORTH);
-
+		bottom.setOpaque(false);
 		bottom.add(bottom2,BorderLayout.CENTER);
+		
 		bottom2.setLayout(new GridLayout(1,5, 10, 1));
 		bottom2.setOpaque(false);
-		bottom.setOpaque(false);
+		
 		bottom1.setOpaque(false);
 		bottom1.setPreferredSize(new Dimension(600,130));
 		bottom1.setLayout(new BorderLayout());
-		JPanel right = new JPanel();
-		JPanel left = new JPanel();
+		left = new JPanel();
+		right = new JPanel();
+
 		left.setOpaque(false);
 		bottom1.add(left,BorderLayout.CENTER);
 		bottom1.add(right, BorderLayout.EAST);
+
 		right.setPreferredSize(new Dimension(200, 100));
 		right.setOpaque(false);
-		Icon cigar = new ImageIcon("images/cigar.gif");
-		JLabel cigarL = new JLabel(cigar);
-		Icon whiskey = new ImageIcon("images/whiskey.png");
-		JLabel whiskeyL = new JLabel(whiskey);
 		right.setLayout(new FlowLayout());
-
 		right.add(whiskeyL);
 		right.add(cigarL);
 		right.setBorder(BorderFactory.createEmptyBorder(0,0,20,0)); // Especially important
 
-		PaintedButton[] cards = new PaintedButton[5];
-		int counter = 1;
-		for(PaintedButton card: cards) {
-			card = new PaintedButton("Bad word " + Integer.toString(counter),white);
-			card.setOpaque(false);
-//			card.setBorder(BorderFactory.createEmptyBorder(0,0,0,0)); // Especially important
-			  card.setHorizontalAlignment(SwingConstants.LEFT);
-			  card.setVerticalAlignment(SwingConstants.TOP);
-			  card.setFont(new Font("Helvetica", Font.BOLD, 16));
-			  card.setMargin(new Insets(20,20, 20, 20));
-//			  card.setMargin(new Insets(1,1,1,1));
-			  card.setBackground(null);
-			  card.setOpaque(false);
-			  card.setBorderPainted(false);
-			card.setContentAreaFilled(false);
-			bottom2.add(card);
-			counter++;
-			card.addActionListener(new ActionListener() {
 
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-			});
-			
-		}
 		//top left bottom right
 		top.setBorder(BorderFactory.createEmptyBorder(40,0,50,0)); // Especially important
 //		top.setBorder(BorderFactory.creatEmpty // Especially important
@@ -154,11 +127,44 @@ public class GamePanel extends PaintedPanel{
 		add(bottom, BorderLayout.SOUTH);
 
 	}
-	
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	public void displayCards(Card[] cardss) {
+		PaintedButton[] cards = new PaintedButton[5];
+		int counter = 1;
+
+		
+		
+		for(PaintedButton card: cards) {
+			card = new PaintedButton("Bad word " + Integer.toString(counter),white);
+			card.setOpaque(false);
+//			card.setBorder(BorderFactory.createEmptyBorder(0,0,0,0)); // Especially important
+			  card.setHorizontalAlignment(SwingConstants.LEFT);
+			  card.setVerticalAlignment(SwingConstants.TOP);
+			  card.setFont(new Font("Helvetica", Font.BOLD, 16));
+			  card.setMargin(new Insets(20,20, 20, 20));
+//			  card.setMargin(new Insets(1,1,1,1));
+			  card.setBackground(null);
+			  card.setOpaque(false);
+			  card.setBorderPainted(false);
+			card.setContentAreaFilled(false);
+			bottom2.add(card);
+			counter++;
+			card.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+			});
+			
+		}
+	}
+
+	
+	public void removeCards() {
+		
+	}
 
 }
